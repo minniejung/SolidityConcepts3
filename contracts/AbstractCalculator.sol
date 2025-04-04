2e2f8665d0cb839cc46de56119b907238d912e96
minniejung <jungmignonne@gmail.com>
2025-04-04 10:50:51 +0900

    solved

--- contracts/AbstractCalculator.sol
+++ contracts/AbstractCalculator.sol
@@ -1,1 +1,25 @@
-abstract contract AbstractCalculator {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ICalculator.sol";
+import "./MathLibrary.sol";
+
+abstract contract AbstractCalculator is ICalculator {
+    using MathLibrary for uint256;
+
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return MathLibrary.add(a, b);
+    }
+
+    function subtract(uint256 a, uint256 b) public pure returns (uint256) {
+        return MathLibrary.subtract(a, b);
+    }
+
+    function multiply(uint256 a, uint256 b) public pure returns (uint256) {
+        return MathLibrary.multiply(a, b);
+    }
+
+    function divide(uint256 a, uint256 b) public pure returns (uint256) {
+        return MathLibrary.divide(a, b);
+    }
+}
--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -1,1 +1,49 @@
-contract Calculator {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./AbstractCalculator.sol";
+// import "./MathLibrary.sol";
+
+contract Calculator is AbstractCalculator {
+    function calculate(
+        uint256 a,
+        uint256 b,
+        string memory operation
+    ) public pure returns (uint256) {
+        bytes32 op = keccak256(abi.encodePacked(operation));
+
+        if (op == keccak256(abi.encodePacked("add"))) {
+            return add(a, b);
+        } else if (op == keccak256(abi.encodePacked("subtract"))) {
+            return subtract(a, b);
+        } else if (op == keccak256(abi.encodePacked("multiply"))) {
+            return multiply(a, b);
+        } else if (op == keccak256(abi.encodePacked("divide"))) {
+            return divide(a, b);
+        } else {
+            revert("Invalid operation");
+        }
+    }
+}
+
+// contract Calculator {
+//     function calculate(
+//         uint256 a,
+//         uint256 b,
+//         string memory operation
+//     ) public pure returns (uint256) {
+//         bytes32 op = keccak256(abi.encodePacked(operation));
+
+//         if (op == keccak256(abi.encodePacked("add"))) {
+//             return MathLibrary.add(a, b);
+//         } else if (op == keccak256(abi.encodePacked("subtract"))) {
+//             return MathLibrary.subtract(a, b);
+//         } else if (op == keccak256(abi.encodePacked("multiply"))) {
+//             return MathLibrary.multiply(a, b);
+//         } else if (op == keccak256(abi.encodePacked("divide"))) {
+//             return MathLibrary.divide(a, b);
+//         } else {
+//             revert("Invalid operation");
+//         }
+//     }
+// }
